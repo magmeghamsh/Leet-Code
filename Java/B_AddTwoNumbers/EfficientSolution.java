0d595fe61012bae7a4f5f87a95c3ff4d072970e2
Meghamsh Gurram <mag0984.meghamsh@gmail.com>
2020-03-11 17:01:30 -0400

    Efficient Solution

--- Java/B_AddTwoNumbers/EfficientSolution.java
+++ Java/B_AddTwoNumbers/EfficientSolution.java
@@ -11,6 +11,29 @@ public class EfficientSolution {
         ListNode l2 = new ListNode(5);
         l2.next = new ListNode(6);
         l2.next.next = new ListNode(4);
-        
+
+        ListNode answerNode = addTwoNumbers(l1, l2);
+        System.out.println("Node value: " + answerNode);
+
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode prev = new ListNode(0);
+        ListNode head = prev;
+        int carry = 0;
+        while(l1 != null || l2 != null){
+            ListNode cur = new ListNode(0);
+            int sum = ((null != l1) ? l1.val : 0) + ((null != l2) ? l2.val : 0) + carry;
+            cur.val = sum%10;
+            carry = sum /10;
+            System.out.println("Current: "+cur.val+"Sum : "+sum+ "Carry: "+carry);
+
+            prev.next = cur;
+            prev = cur;
+            l1 = (l1 == null) ? l1 : l1.next;
+            l2 = (l2 == null) ? l2 : l2.next;
+        }
+        return head.next;
     }
 }
+
