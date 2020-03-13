80ff96c4df767ae9ef8efae188379052de7d1fd3
Vinay Abhishek Manchiraju <vinay@ip-10-4-150-110.ec2.internal>
2020-03-12 23:56:20 -0400

    MySolution

--- Java/E_LongestPalindromicSubstring/MySolution.java
+++ Java/E_LongestPalindromicSubstring/MySolution.java
@@ -1,4 +1,48 @@
 package LeetCode.E_LongestPalindromicSubstring;
 
 public class MySolution {
+
+    public static void main(String []args){
+        System.out.println("Result "+longestPalindrome("cabbace"));
+    }
+
+    public static String  longestPalindrome(String input) {
+        int length = input.length();
+        String result = "";
+        if (length == 1){
+            return input;
+        }
+        for(int i = length; i > 1; i --){
+            int currLen = i -1 ;
+            int endLen = currLen;
+            for(int j = 0; j < length; j++){
+                for(int k = endLen ; k >=j; k --){
+                    if( (k-j) == currLen ){
+                        result = input.substring(j,k);
+                        if(isPalindrome(result)){
+                            return result;
+                        }else{
+                            break;
+                        }
+                    }
+                }
+                endLen+=1;
+                if(endLen >= length){
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    private static boolean isPalindrome (String s){
+        StringBuilder sb = new StringBuilder(s);
+        if(sb.reverse().toString().equals(s)){
+            return true;
+        }
+        return false;
+    }
+
+
+
 }
--- Java/E_LongestPalindromicSubstring/README.md
+++ Java/E_LongestPalindromicSubstring/README.md
@@ -17,13 +17,23 @@
 ### Pseudo code:
 
 **My Solution:**
+c a b b a c e 
+
+Start with the string itself and performing the palindrome check for every possible substring in decrements of length
+
+c a b b a c e | c a b b a c | c a b b a | c a b b
+              | a b b a c e | a b b a c | a b b a
+                            | b b a c e | b b a c
+                                        | b a c e
 
 _Step 1:_
 
 _Step 2:_
 
 **_Time complexity:_**  
-
+For a string of 7 characters
+We end up with  16 combinations
+and for 1000 characters we end up with 2200 combinations -> O(2n) which is  O(n)
 **_Space Complexity:_** 
 
 **Why is it failing?**
